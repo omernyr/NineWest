3fca7eb40ce01cb238bd066e1ce6ecb2c11f637e
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-25 12:02:14 +0300

    add CategoryCollectionViewModel

---
+++ NineWest/ViewModel/CategoryCollectionViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryCollectionViewModel.swift
+//  NineWest
+//
+//  Created by macbook pro on 25.05.2023.
+//
+
+import Foundation
+
+class CategoryCollectionViewModel {
+    
+    // Variables
+    public let data = ["Yeni", "Ayakkabı", "Sneaker", "Topuklular", "Sandalet", "Terlik", "Çanta"]
+    
+    
+    
+}
