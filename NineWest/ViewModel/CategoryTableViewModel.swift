bbe1d20c66139f5234f6bf36303cca9c41dd3e21
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-05-25 12:50:16 +0300

    upgrade CategoryTableViewModel

--- NineWest/ViewModel/CategoryTableViewModel.swift
+++ NineWest/ViewModel/CategoryTableViewModel.swift
@@ -6,9 +6,25 @@
 //
 
 import Foundation
+import UIKit
 
 class CategoryTableViewModel {
     
+    public var categoryNames: [ProdCategory] = []
     
+    func getData(_ myTableView: UITableView ) {
+        
+        APICaller.getAllCategorys { result in
+            switch result {
+            case .success(let success):
+                DispatchQueue.main.async {
+                    self.categoryNames = success
+                    myTableView.reloadData()
+                }
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
     
 }
