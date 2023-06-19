10461cf22175f5c508e286349115cd61e1d8b0ca
omernyr <omer.nayir.mdbf17@iste.edu.tr>
2023-06-19 15:55:59 +0300

    add CollectionViewCell

--- NineWest/View/ProductCollectionViewCell/ProductCollectionViewCell.swift
+++ NineWest/View/ProductCollectionViewCell/ProductCollectionViewCell.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ProductCollectionViewCell: UICollectionViewCell {
-
-    static let identifier = "omernyr/NineWest"
+    
+    @IBOutlet var prodImageView: UIImageView!
+    @IBOutlet var prodRateLabel: UILabel!
+    @IBOutlet var prodNameLabel: UILabel!
+    @IBOutlet var prodPriceLabel: UILabel!
+    @IBOutlet var favoriteButton: UIButton!
+   
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        prodImageView.image = UIImage(systemName: "camera")
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        // Hücre özelleştirmelerini burada yapabilirsiniz
     }
 
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    @IBAction func didTappedFavoriteButton(_ sender: Any) {
+        
+    }
+    
 }
